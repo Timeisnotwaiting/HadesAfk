409401f230700a198a2d06c31759f1fd0c6a9258
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-16 11:19:26 +0530

    Update broadcast.py

--- Hades/broadcast.py
+++ Hades/broadcast.py
@@ -55,3 +55,35 @@ async def broadcast(_, message):
         )
     except:
         pass
+
+async def info(_, m):
+    if len(m.command) == 2:
+        lel = int(m.text.split(None, 1)[1])
+        if str(lel)[0] == "-":
+            id = lel
+        else:
+            omfoo = "-" + str(lel)
+            id = int(omfoo)
+
+    getter = await _.get_chat(id)
+    try:
+        username = getter.username
+    except:
+        username = "None"
+    try:
+        link = getter.invite_link
+    except:
+        link = "None"
+    await m.reply(f"Group name :- {getter.title}\n\nInvite link :- {link}\n\nUsername :- @{username}")
+
+async def schats(_, m):
+    chats = await get_served_chats()
+    msg = ""
+    NOTED = []
+    for i in chats:
+        if i in NOTED:
+            continue
+        NOTED.append(i)
+        i = str(i)
+        msg += f"\n<code>{i}</code>"
+    await m.reply(f"**Served chats** :-\n{msg}\n\n**Count** :- {len(NOTED)}")
